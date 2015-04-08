c785f8978cb6c57b04e14c90b5bd36ba1ce12304
gzpgg3x <gzpgg3x@yahoo.com>
2015-04-08 14:31:02 -0400

    prior7.3.CreatingaDetailsPage
    prior7.3.CreatingaDetailsPage

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+# from django.contrib import admin
+from myApp import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^myApp/', include('myApp.urls')),    
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name="index"),    
 )
---
+++ myProject/myApp/views (prior7.2.2).py
@@ -0,0 +1,23 @@
+# from django.shortcuts import render
+
+# def index(request):
+#     render ("Hello World")
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from myApp.models import Category, Page
+
+# def index(request):
+#     return HttpResponse("Rango says hey there world!")
+
+def index(request):
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'myApp/index.html', context_dict)
